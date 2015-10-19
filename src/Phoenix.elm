e27352ff5104e1979f754895a9f1c063140c8fbd
Kristofer Svärd <kristofer.svard@gmail.com>
2015-10-19 19:52:41 +0200

    Add comments

--- src/Phoenix.elm
+++ src/Phoenix.elm
@@ -1,5 +1,5 @@
-module Phoenix 
-  ( defaultSocketParams
+module Phoenix
+  ( defaultSocketOptions
   , socket
   , connect
   , channel
@@ -8,13 +8,17 @@ module Phoenix
   , on
   , Socket
   , Channel
-  , Transport
-  , SocketParams
+  , SocketOptions
   , Transport(..)
   ) where
 
-{-| Docs 
-@docs Socket, Channel, socket, channel, connect, join, push, on 
+{-|
+
+# Connecting
+@docs socket, connect, channel, join, defaulSocketOptions, SocketOptions
+
+# Sending and receiving
+@docs push, on
 -}
 
 import Task
@@ -25,47 +29,72 @@ type Socket = Socket
 {-| Channel -}
 type Channel = Channel
 
+type alias Topic = String
+type alias Event = String
+type alias EndPoint = String
+
+{-| A type representing the transport protocol between the client and server.
+-}
 type Transport
   = WebSocket
   | LongPoll
 
-{-| SocketParams -}
-type alias SocketParams =
+{-| Set optional parameters for the socket connection.
+-}
+type alias SocketOptions =
   { transport : Transport
   , heartbeatInterval : Int
   , reconnectAfter : Int
   , longpollerTimeout : Int
   }
 
-{-| defaultsocketparams -}
-defaultSocketParams : SocketParams
-defaultSocketParams = 
+{-| Default socket parameters. They are the same as specified in phoenix.js.
+    { transport = WebSocket
+    , heartbeatInterval = 30000
+    , reconnectAfter = 10000
+    , longpollerTimeout = 20000
+    }
+-}
+defaultSocketOptions : SocketOptions
+defaultSocketOptions =
   { transport = WebSocket
   , heartbeatInterval = 30000
   , reconnectAfter = 10000
   , longpollerTimeout = 20000
   }
 
-{-| socket -}
-socket : String -> SocketParams -> Socket
+{-| Initializes the socket. EndPoint is the websocket endpoint and can be given
+with or whitout protocol and host.
+  * "ws://example.com/ws"
+  * "wss://example.com"
+  * "/ws"
+-}
+socket : EndPoint -> SocketOptions -> Socket
 socket = Native.Phoenix.socket
 
-{-| connect -}
+{-| Attempt to connect to the server.
+ -}
 connect : Socket -> Task.Task x Socket
 connect = Native.Phoenix.connect
 
-{-| channel -}
-channel : String -> Socket -> Task.Task x Channel
+{-| Create a channel with a given topic over the socket.
+-}
+channel : Topic -> Socket -> Task.Task x Channel
 channel = Native.Phoenix.channel
 
-{-| join -}
+{-| Attempt to join the given channel. Messages sent by the server on a
+successful join will be delivered to the specified address.
+-}
 join : Signal.Address String -> Channel -> Task.Task x Channel
 join = Native.Phoenix.join
 
-{-| push -}
-push : String -> String -> Channel -> Task.Task x ()
+{-| Pushes a message over the channel to the server as event.
+ -}
+push : Event -> String -> Channel -> Task.Task x ()
 push = Native.Phoenix.push
 
-{-| on -}
-on : String -> Signal.Address String -> Channel -> Task.Task x ()
+{-| Subscribes to event on the channel. Received messages will be delivered to
+the specified address.
+-}
+on : Event -> Signal.Address String -> Channel -> Task.Task x ()
 on = Native.Phoenix.on
